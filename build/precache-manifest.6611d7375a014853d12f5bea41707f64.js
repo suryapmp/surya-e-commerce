1ad75ad3ae87e3e25a835a3012bfa81f98ff8846
surya <72649807+suryapmp@users.noreply.github.com>
2021-09-21 22:13:24 +0530

    Add files via upload

---
+++ build/precache-manifest.6611d7375a014853d12f5bea41707f64.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cadcc31699558e08d9c3a10429b65467",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd78267dd99644f9a08d",
+    "url": "/static/css/main.ff56beb9.chunk.css"
+  },
+  {
+    "revision": "2dd7aa7d60f99d4e6bef",
+    "url": "/static/js/2.1577b022.chunk.js"
+  },
+  {
+    "revision": "dcd1d80362aef046a5450470cca1218a",
+    "url": "/static/js/2.1577b022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd78267dd99644f9a08d",
+    "url": "/static/js/main.75ef0bb8.chunk.js"
+  },
+  {
+    "revision": "b14ea0db6cdb287a0925",
+    "url": "/static/js/runtime-main.f8c9dd52.js"
+  },
+  {
+    "revision": "6bd7decf6fabd390a8868a3153d0f9c7",
+    "url": "/static/media/logo.6bd7decf.png"
+  },
+  {
+    "revision": "19190d053a8b13a3d9f528c9f7628817",
+    "url": "/static/media/shopMens.19190d05.jpg"
+  },
+  {
+    "revision": "c9c60ae5dd1c192f2617996f1b693582",
+    "url": "/static/media/shopWomens.c9c60ae5.jpg"
+  }
+]);
